5c81a61ec51bd8ccaebb963097b9cb4ce9ce1809
kenkwon <supken@naver.com>
2024-11-14 23:23:15 +0900

    PostService.ts에서 fetch URL을 템플릿 리터럴로 변경

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -2,7 +2,7 @@ import { Post } from '@/types/Post';
 
 export const getPosts = async (): Promise<Post[]> => {
   const response = await fetch(
-    'https://wingman.sekc.or.kr/posts?type=wait&searchKeyword=&pointerId=9007199254740991&pageSize=1000'
+    `https://wingman.sekc.or.kr/posts?type=wait&searchKeyword=&pointerId=9007199254740991&pageSize=1000`
   );
   return response.json();
 };
